e07f14733ebdfcaa9426a1a6b3cd3565b86cc42d
Rushikesh Dadas <rushikeshdadas03@gmail.com>
2023-11-07 12:35:15 +0530

    utils, cors, cookie-parser

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error => {
+    console.log("MONGODB CONNECTION FAILED", error)
+}))
 
 /*
 import express from "express"

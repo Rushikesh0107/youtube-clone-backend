d1ee4d9b32d363bd67c18b402c0446229469169f
Rushikesh0107 <rushikeshdadas03@gmail.com>
2023-11-20 23:33:56 +0530

    DB connected, finished set-up, wrote first controller

--- src/app.js
+++ src/app.js
@@ -15,5 +15,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes import
+
+import userRouter from './routes/user.routes.js'
+
+
+// Routes declaration
+
+app.use("/api/v1/users", userRouter)
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import {app} from "./app.js";
 
 dotenv.config({
     path: "./.env"
